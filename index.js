362789896a36cf93b00a88313ebd9f3acc9a9182
giegiey23 <fahmi.kkpg@gmail.com>
2025-11-11 15:08:53 +0800

    upt: fixed always on top

--- index.js
+++ index.js
@@ -1863,8 +1863,8 @@ try {
                 width: 0,
                 height: 0,
                 backgroundColor: '#000000',
-                //alwaysOnTop: true,
-                //autoHideMenuBar: true,
+                alwaysOnTop: true,
+                autoHideMenuBar: true,
                 fullscreenable: false,
                 resizable: false,
                 moveable: false,
@@ -1890,8 +1890,8 @@ try {
                 width: 0,
                 height: 900,
                 backgroundColor: '#302d2d',
-                //alwaysOnTop: true,
-                //autoHideMenuBar: true,
+                alwaysOnTop: true,
+                autoHideMenuBar: true,
                 fullscreenable: false,
                 resizable: false,
                 moveable: false,
@@ -1959,10 +1959,10 @@ try {
             })
 
             //hide menu bar
-            //win.setSkipTaskbar(true)
-            //win.setAlwaysOnTop(true)
-            //win2.setSkipTaskbar(true)
-            //win2.setAlwaysOnTop(true)
+            win.setSkipTaskbar(true)
+            win.setAlwaysOnTop(true)
+            win2.setSkipTaskbar(true)
+            win2.setAlwaysOnTop(true)
             win.setMenuBarVisibility(false)
             win2.setMenuBarVisibility(false)
             Menu.setApplicationMenu(null)
